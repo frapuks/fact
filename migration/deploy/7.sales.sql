996310bcda36c5da7f187199aa88e9a3f985eda1
francois <francoisgrunert@gmail.com>
2023-01-28 17:57:23 +0100

    petit coup de pouce

--- migration/deploy/7.sales.sql
+++ migration/deploy/7.sales.sql
@@ -7,9 +7,9 @@ CREATE TYPE sales AS (date DATE, nb_invoices INTEGER, total NUMERIC);
 CREATE FUNCTION sales_by_dates(date_from DATE, date_to DATE) RETURNS SETOF sales AS $$
 	BEGIN
 		RETURN QUERY (
-			SELECT currentDate::DATE, COUNT(invoice_recap.id)::integer AS nb_invoices, SUM(invoice_recap.total)
+			SELECT currentDate::DATE, COUNT(invoice_recap.id)::integer AS nb_invoices, COALESCE(SUM(invoice_recap.total),0)
 			FROM generate_series(date_from::DATE, date_to::DATE, '1day') AS currentDate
-			JOIN invoice_recap ON currentDate = issued_at::date
+			FULL JOIN invoice_recap ON currentDate = issued_at::date
 			GROUP BY currentDate
 		);
 	END
